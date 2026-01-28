abc4944c0520d242d9fac6a359736750d4eb8f8f
AmitBiswas1402 <amit.142biswas@gmail.com>
2026-01-28 11:34:48 +0530

    Add event listing features: create EventCard and Navbar components, implement event data structure, and enhance layout with LightRays effect

--- components/ExploreButton.tsx
+++ components/ExploreButton.tsx
@@ -1,17 +1,21 @@
-"use client"
+"use client";
 
-import Image from "next/image"
+import Image from "next/image";
 
 const ExploreButton = () => {
   return (
     <button className="mt-7 mx-auto" id="explore-btn" type="button">
-        <a href="#events">
-            Explore Events 
-            <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
-        </a>
-
+      <a href="#events">
+        Explore Events
+        <Image
+          src="/icons/arrow-down.svg"
+          alt="arrow-down"
+          width={24}
+          height={24}
+        />
+      </a>
     </button>
-  )
-}
+  );
+};
 
-export default ExploreButton
+export default ExploreButton;
